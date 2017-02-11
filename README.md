849e87dfd1b0792f3b2fc8ce0f0868a2de9ab8c1
Sheng-Hao <shenghah@andrew.cmu.edu>
2017-02-11 06:03:28 -0500

    Added API usage file.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Movie-Matchmaker
 Tartanhacks 2017 Project
+
+Files:
+    description_parser.py - python file that shows how to use Alchemy Language API
---
+++ description_parser.py
@@ -0,0 +1,37 @@
+import json
+from os.path import join, dirname
+from watson_developer_cloud import AlchemyLanguageV1
+
+# bluemix AlchemyApi API key
+api_key = '6487efd8fd5427eec5345d11fa679e3293370dd2'
+
+# construct API object from key
+alchemy_language = AlchemyLanguageV1(api_key=api_key)
+
+# store URL to examine
+stored_url = 'https://developer.ibm.com/watson/blog/2015/11/03/price-reduction-for-watson-personality-insights/'
+
+# example text for testing
+test_text = "Daunted by the singular tastes and dark secrets of the handsome, tormented young entrepreneur Christian Grey, Anastasia Steele has broken off their relationship to start a new career with a Seattle Independent Publishing House (SIP); but desire for Christian still dominates her every waking thought, and when he proposes a new arrangement, Anastasia cannot resist. They rekindle their searing sexual affair, and Anastasia learns more about the harrowing past of her damaged, driven and demanding Fifty Shades. \n"\
+            "While Christian wrestles with his inner demons, Anastasia must confront the anger and envy of the women who came before her, and make the most important decision of her life. All about the sex."
+
+# get dictionary of data from text
+text_dict = alchemy_language.emotion(text = test_text, language = 'english')
+
+# get dictionary from text_dict
+emotion_dict = text_dict['docEmotions']
+
+# convert individual emotions percents to floats
+(joy, disgust, anger, fear, sadness) = (float(emotion_dict["joy"]),
+                                        float(emotion_dict["disgust"]),
+                                        float(emotion_dict["anger"]),
+                                        float(emotion_dict["fear"]),
+                                        float(emotion_dict["sadness"]))
+
+# recalculate emotion percents and store them back in emotion_dict as floats
+total = (joy + disgust + anger + fear + sadness)
+emotion_dict["joy"] = joy/total
+emotion_dict["disgust"] = disgust/total
+emotion_dict["anger"] = anger/total
+emotion_dict["fear"] = fear/total
+emotion_dict["sadness"] = sadness/total
